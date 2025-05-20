41b428536ed2c9fba90050beec6573cd1cfbff93
Akshay Lakhi <akshay.lakhi@mindfort.tech>
2025-05-20 16:08:36 +0200

    added health research network agent and updated structure of the repo for separate playground.py files

---
+++ health_research_network_agent/playground.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+from agno.agent import Agent
+
+load_dotenv(dotenv_path="/Users/lakhi/Developer/uni-studAsst-projects/ai_agents_ws/.env")
+
+health_research_network_agent = Agent(
+    
+)
